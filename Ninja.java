75bb58bc5b0d6227ffb7732e976b35acf8e26e61
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-08 21:15:41 -0300

    Construtores do Ninja

--- Ninja.java
+++ Ninja.java
@@ -2,6 +2,14 @@ public class Ninja {
     private String nome;
     private int idade;
     private String vila;
+    public Ninja() {
+
+    }
+    public Ninja(String nome, int idade, String vila) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vila = vila;
+    }
     public String getNome() {
         return nome;
     }
