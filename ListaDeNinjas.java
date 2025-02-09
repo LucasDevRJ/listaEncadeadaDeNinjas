7ef9f916c7d1efa17dc8c65426b6fe08eba71e49
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-08 21:23:39 -0300

    Método para adicionar Ninja.

--- ListaDeNinjas.java
+++ ListaDeNinjas.java
@@ -2,4 +2,14 @@ import java.util.LinkedList;
 
 public class ListaDeNinjas {
     private LinkedList<Ninja> listaDeNinjas = new LinkedList<>();
+
+    public ListaDeNinjas() {
+
+    }
+
+
+    public void adicionaNinja(Ninja ninja) {
+        listaDeNinjas.add(ninja);
+        System.out.println("O ninja " + ninja + " foi adicionado com sucesso.");
+    }
 }
