ba6faf2d777b0407cfbca39da44581f2e5db60d7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-08 21:14:46 -0300

    Encapsulando os atributos

--- Ninja.java
+++ Ninja.java
@@ -1,2 +1,27 @@
 public class Ninja {
+    private String nome;
+    private int idade;
+    private String vila;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getVila() {
+        return vila;
+    }
+
+    public void setVila(String vila) {
+        this.vila = vila;
+    }
 }
